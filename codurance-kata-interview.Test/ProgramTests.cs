4910d3b8e0b14394d79f5965a48ae2c8c9b9bbb8
Joana <joanajanefdez@gmail.com>
2025-11-29 09:35:41 +0100

    state after interview

--- codurance-kata-interview.Test/ProgramTests.cs
+++ codurance-kata-interview.Test/ProgramTests.cs
@@ -3,8 +3,77 @@ namespace codurance_kata_interview.Test;
 public class ProgramTests
 {
     [Fact]
-    public void InitialTest()
+    public void RobotShouldExecuteCommandReceivedAndReturnCurrentPosition()
     {
-        Assert.True(true);
+        //given
+        var expectedPosition = "0:0:N";
+        Robot robot = new Robot();
+
+        //when
+        var command = "";
+        var position = robot.Execute(command);        
+
+        //then
+        Assert.Equal(expectedPosition, position);
+    }
+
+    [Fact]
+    public void RobotShouldRotateWhenReceivingDirectionLeft()
+    {
+        //given
+        var expectedPosition = "0:0:W";
+        Robot robot = new Robot();
+
+        //when
+        var command = "L";
+        var position = robot.Execute(command);
+
+        //then
+        Assert.Equal(expectedPosition, position);
+    }
+
+    [Fact]
+    public void RobotShouldRotateWhenReceivingDirectionRight()
+    {
+        //given
+        var expectedPosition = "0:0:E";
+        Robot robot = new Robot();
+
+        //when
+        var command = "R";
+        var position = robot.Execute(command);
+
+        //then
+        Assert.Equal(expectedPosition, position);
+    }
+
+    [Fact]
+    public void RobotShouldMoveWhenReceivingMoveCommand()
+    {
+        //given
+        var expectedPosition = "0:1:N";
+        Robot robot = new Robot();
+
+        //when
+        var command = "M";
+        var position = robot.Execute(command);
+
+        //then
+        Assert.Equal(expectedPosition, position);
+    }
+
+    [Fact]
+    public void RobotShouldMoveWestWhenReceivingLeftAndMoveCommand()
+    {
+        //given
+        var expectedPosition = "-1:0:W";
+        Robot robot = new Robot();
+
+        //when
+        var command = "LM";
+        var position = robot.Execute(command);
+
+        //then
+        Assert.Equal(expectedPosition, position);
     }
 }
---
+++ codurance-kata-interview/Robot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codurance_kata_interview
+{    
+    public class Robot
+    {
+        public int x;
+        public int y;
+        public string direction; 
+
+        public Robot() 
+        { 
+            x = 0;
+            y = 0;
+            direction = "N";
+        }
+
+        public string Execute(string command)
+        {
+            var commands = command.ToArray();     
+
+            foreach (var order in commands)
+            {
+                
+            }
+
+            if (command.Contains("L"))
+            {                
+                direction = "W";
+            }
+
+            if (command.Contains("R"))
+            {
+                direction = "E";
+            }
+
+            if (command.Contains("M"))
+            {
+                if(direction == "N")
+                {
+                    y++;
+                }
+                if(direction == "W")
+                {
+                    x--;
+                }
+                if(direction == "E")
+                {
+                    x++;
+                }
+                if(direction == "S")
+                {
+                    y--;
+                }                    
+            }
+
+
+            return "0:0:N";
+        }
+    }  
+    
+}
